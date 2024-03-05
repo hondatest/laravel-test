23df3eeb0854b8685f00044678935900d8f6d206
honda <m.honda@ynoc.co.jp>
2024-03-05 13:32:59 +0900

    add: 各商品ページのタイトルを追加
    各商品ページのタイトルを表示していないため。
    
    以下はタイトルを追加した各商品ページ。
    ・商品一覧
    ・商品投稿
    ・商品編集
    ・商品詳細

--- resources/views/product/edit.blade.php
+++ resources/views/product/edit.blade.php
@@ -4,12 +4,13 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
+  <title>商品編集</title>
 </head>
 <body>
   <form action="{{ route('products.update', [ 'product' => $product->id ]) }}" method="post">
     @method('put')
     @csrf
+    <h1>商品編集</h1>
 		<div>
 			<label for="name">名前</label>
 			<input type="text" name="name" value="{{ old('name', $product->name) }}">
--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -4,9 +4,10 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
+  <title>商品一覧</title>
 </head>
 <body>
+  <h1>商品一覧</h1>
   <a href="{{ route('products.create') }}">商品投稿ページへ</a>
   <ul>
   @foreach ($products as $product)
