99be8577c482430f7b7d436e0b4bd1d06e27ab82
honda <m.honda@ynoc.co.jp>
2024-03-07 09:53:54 +0900

    add: クチコミテーブルのuser_idカラムとproduct_idカラムにユニークキー制約を追加
    保存するデータの重複を防ぐため。

--- database/migrations/2024_03_06_003155_create_reviews_table.php
+++ database/migrations/2024_03_06_003155_create_reviews_table.php
@@ -27,6 +27,7 @@ return new class extends Migration
                 ->on('users')
                 ->cascadeOnUpdate()
                 ->cascadeOnDelete();
+            $table->unique(['product_id', 'user_id']);
         });
     }
 
